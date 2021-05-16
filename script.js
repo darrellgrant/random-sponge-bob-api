a5e95583c9d394d5490f472cb7cfed91357407e9
Darrell Grant <dlsan3000@gmail.com>
2021-05-15 20:45:30 -0400

    added .gitignore, data.php

--- script.js
+++ script.js
@@ -2,22 +2,36 @@ let button = document.querySelector("#button");
 button.addEventListener("click", function () {
   location.reload();
 });
-let showcase = document.querySelector("#showcase");
 
-let request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://api.giphy.com/v1/gifs/random?api_key=3FZm8c2tZqF58iHHVYXNjUAIIKfLXOxU&tag=spongebob&rating=g"
-);
-request.onload = function () {
-  let response = request.response;
-  let parsedData = JSON.parse(response);
-  console.log(parsedData);
-  let originalUrl = parsedData.data.images.original.url;
-  console.log(originalUrl);
+function getData(callback) {
+  fetch("data.php")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      callback(data);
+    });
+}
 
-  let gif = document.createElement("img");
-  gif.setAttribute("src", originalUrl);
-  showcase.appendChild(gif);
-};
-request.send();
+getData(function (myData) {
+  let myAppiKey = myData;
+  let showcase = document.querySelector("#showcase");
+
+  let request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://api.giphy.com/v1/gifs/random?api_key=${myAppiKey}&tag=spongebob&rating=g`
+  );
+  request.onload = function () {
+    let response = request.response;
+    let parsedData = JSON.parse(response);
+    console.log(parsedData);
+    let originalUrl = parsedData.data.images.original.url;
+    console.log(originalUrl);
+
+    let gif = document.createElement("img");
+    gif.setAttribute("src", originalUrl);
+    showcase.appendChild(gif);
+  };
+  request.send();
+});
